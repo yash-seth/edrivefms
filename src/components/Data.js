a541f0a410f03d9b3e3ca676a7d76959746b589d
Yash Seth <yashseth2002@gmail.com>
2021-11-28 19:39:57 +0530

    refactored search functionality to deal with folder view

--- src/components/Data.js
+++ src/components/Data.js
@@ -54,7 +54,6 @@ function Data(props) {
                     break;
                 }
             }  
-            if(files.length===0)props.settotalSize(0);
             storageHandler();
             setDeleteModalState(null);
             setDeleting(false);
@@ -77,14 +76,12 @@ function Data(props) {
     }
 
     const storageHandler =() =>{
-    if(folderView===false){
     files.map((file) => {
         if(file.data.type !== "folder") {
             props.settotalSize(totalBytes+=file.data.size);
         }
         if(fileCounter(files)===0) props.settotalSize(0);
     })
-    }
     }
 
     const fileCounter =(files)=>
@@ -100,6 +97,88 @@ function Data(props) {
 
     return props.searchState ? 
     (
+        !folderView ? 
+        <div className="data">
+            <div className="data__header">
+                <div className="data__headerLeft">
+                    <p>My Drive</p>
+                    <button className="Root-Btn" onClick={()=>{props.setFolderID("/");setfolderView(false)}}>Root</button>
+                </div>
+                <div className="data__headerRight">
+                    <ListIcon />
+                    <InfoOutlinedIcon />
+                </div>
+            </div>
+            <div className="data__content">
+                <div className="data_list">
+                    <div className="detailsrow">
+                        <p><b>Name <ArrowDownwardIcon /></b></p>
+                        <p><b>Owner</b></p>
+                        <p><b>Created At</b></p>
+                        <p><b>File Size</b></p>
+                        <p><b>Share</b></p>
+                        <p><b>Delete</b></p>
+                    </div>
+                    {
+                        files.map((file) => {
+                                return file.data.filename.toLowerCase().includes(String(props.searchValue))? <div className="detailsrow">
+                                <p> <a href={file.data.fileURL} target="_blank">
+                                    <InsertDriveFileIcon />{file.data.filename}</a></p>
+                                <p>{file.data.username}</p>
+                                <p>{new Date(file.data.timestamp?.seconds * 1000).toUTCString()}</p>
+                                <p>{formatBytes(file.data.size)}</p>
+                                <p><button className="delete" onClick={() => setshareModalState(file.data.fileURL)}><ShareIcon/></button></p>
+                                
+                                <Modal open={file.data.fileURL===shareModalState} onClose ={shareClose}>
+                                    <div className="modal_pop">
+                                        <form>
+                                            <div className="modalHeading">
+                                                <h3>Copy the Below Link to Share</h3>
+                                            </div>
+                                            <div className="modalBody">
+                                                {
+                                                    sharing ? (<p className="uploading">Copying to Clipboard</p>) : (
+                                                        <> <label>
+                                                            <input className="link" type ="text" value={shareModalState} disabled/>
+                                                            <CopyToClipboard text={shareModalState}><button className="delete" onClick={ ()=> CopyClipboard()}><ContentCopyIcon/></button></CopyToClipboard>
+                                                            <button className="Close" onClick={() => setshareModalState(false)}>Close</button>
+                                                            </label>
+                                                        </>)
+                                                }
+                                            </div>
+                                            
+                                        </form>
+                                    </div>    
+                                </Modal>
+                                <div className="del"><p><button className="delete" onClick={() => setDeleteModalState(file.data.fileURL)}><DeleteIcon /></button></p></div>
+                                <Modal open={file.data.fileURL===deleteModalState} onClose={handleClose}>
+                                    <div className="modal_pop">
+                                        <form>
+                                            <div className="modalHeading">
+                                                <h3>Confirm Delete Operation?</h3>
+                                            </div>
+                                            <div className="modalBody">
+                                                {
+                                                    deleting ? (<p className="uploading">Deleting</p>) : (
+                                                        <>  <label>
+                                                            <button className="Yes" onClick={() =>{ deleteFile(file.data.filename, file.data.fileURL,file.data.size);props.settotalSize(props.totalSize-file.data.size)}}>Yes</button>
+                                                            <button className="No" onClick={() => setDeleteModalState(null)}>No</button>
+                                                        </label>
+                                                        </>)
+                                                }
+                                            </div>
+                                        </form>
+                                    </div>
+                                </Modal>
+                                </div> : null
+                        })
+                    }
+
+                </div>
+
+            </div>
+        </div>
+        :
         <div className="data">
             <div className="data__header">
                 <div className="data__headerLeft">
@@ -118,7 +197,7 @@ function Data(props) {
                         files.map((file) => {
                             if( file.data.parentFolderID === props.folderID && file.data.type === "folder"){
                                 return file.data.filename.toLowerCase().includes(String(props.searchValue))? <>
-                                <a onClick={()=> {props.setFolderID(`${file.data.folderID}`);setfolderView(false)}}>
+                                <a onClick={()=> {props.setFolderID(`${file.data.folderID}`);setfolderView(true)}}>
                                     <div className="data_file">
                                         <p>{file.data.filename}</p>
                                     </div>
@@ -207,7 +286,7 @@ function Data(props) {
             <div className="data__header">
                 <div className="data__headerLeft">
                     <p>My Drive</p>
-                    <button className="Root-Btn" onClick={()=>{props.setFolderID("/")}}>Root</button>
+                    <button className="Root-Btn" onClick={()=>{props.setFolderID("/");setfolderView(false)}}>Root</button>
                 </div>
                 <div className="data__headerRight">
                     <ListIcon />
@@ -223,7 +302,7 @@ function Data(props) {
                             if( file.data.parentFolderID === props.folderID && file.data.type === "folder"){
                                 // console.log(file.data.filename);
                                 return <>
-                                    <a onClick={()=> {props.setFolderID(`${file.data.folderID}`)}}>
+                                    <a onClick={()=> {props.setFolderID(`${file.data.folderID}`);setfolderView(true)}}>
                                         {/* {console.log(props.folderID)} */}
                                         <div className="data_file">
                                             <p>{file.data.filename}</p>
